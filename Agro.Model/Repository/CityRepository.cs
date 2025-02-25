72c1c479ddc553968daf1dbe42b22c5abee96edc
Kanat Batyrkhanov <kanat_batyrkhanov@epam.com>
2025-02-25 15:32:48 +0500

    cityrepository formatted

--- Agro.Model/Repository/CityRepository.cs
+++ Agro.Model/Repository/CityRepository.cs
@@ -1,19 +1,13 @@
 ﻿using Agro.Model.Data;
 using Agro.Model.Entities;
 using Agro.Model.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Agro.Model.Repository
-{
-    public class CityRepository: AsyncRepository<City>, ICityRepository
-    {
-        public CityRepository(ApplicationDbContext dbContext) : base(dbContext)
-        {
-            _dbContext = dbContext;
-        }
-    }
-}
+namespace Agro.Model.Repository;
+
+public class CityRepository: AsyncRepository<City>, ICityRepository
+  {
+      public CityRepository(ApplicationDbContext dbContext) : base(dbContext)
+      {
+          _dbContext = dbContext;
+      }
+  }
